b5bdd497270d3f3bb0f29b9f42c5677f0f4ddd25
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-07-16 17:11:16 +0200

    feat(db): more tables
    service_permission referencing either users or groups to services
    
    group_invitation that carry potential user, remaining uses, and expire after values
    
    trigger to give a default expiration date.
    
    Service and groups table adds owner with reference to userid for permissions

--- sql/docker-entrypoint-initdb.d/structure.sql
+++ sql/docker-entrypoint-initdb.d/structure.sql
@@ -1,39 +1,78 @@
-CREATE TABLE users
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        username VARCHAR(255) UNIQUE NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        pwdhash TEXT NOT NULL,
-        two_fa_ref TEXT,
-    );
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    pwdhash TEXT NOT NULL,
+    two_fa_ref TEXT,
+);
 
-CREATE TABLE groups 
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        groupname VARCHAR(255) UNIQUE,
+CREATE TABLE groups (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    groupname VARCHAR(255) UNIQUE,
+    owner INTEGER UNIQUE,
+    Foreign Key (owner) REFERENCES users (id)
+);
 
-    );
+CREATE TABLE service (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    servicename TEXT NOT NULL,
+    loginname TEXT NOT NULL,
+    loginpassword TEXT NOT NULL,
+    twoFAref TEXT,
+    owner INTEGER not NULL,
+    FOREIGN KEY (owner) REFERENCES users (id)
+);
 
-CREATE TABLE perms
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        permname VARCHAR(255) UNIQUE NOT NULL,
-    );
+CREATE TABLE service_permission (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_id INTEGER,
+    group_id INTEGER,
+    service_id INTEGER NOT NULL,
+    UNIQUE (user_id, service_id),
+    UNIQUE (group_id, service_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (group_id) REFERENCES groups (id),
+    FOREIGN KEY (service_id) REFERENCES service (id),
+    Check (
+        (
+            user_id IS NOT NULL
+            AND group_is IS null
+        )
+        or (
+            user_id IS NULL
+            AND group_is IS NOT NULL
+        )
+    )
+);
 
-CREATE TABLE authentications
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        servicename TEXT NOT NULL, 
-        loginname TEXT NOT NULL,
-        loginpassword TEXT NOT NULL,
-        twoFAref TEXT
-    );
+CREATE TABLE user_groups (
+    user_id INTEGER,
+    group_id INTEGER,
+    PRIMARY KEY (user_id, group_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (group_id) REFERENCES groups (id)
+);
 
-CREATE TABLE user_groups 
-    (
-        user_id INTEGER,
-        group_id INTEGER,
-        PRIMARY KEY (user_id, group_id),
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        FOREIGN KEY (group_id) REFERENCES groups(id)
-    );
+CREATE TABLE group_invitation (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    group_id INTEGER NOT NULL,
+    user_id INTEGER,
+    expire_after TIMESTAMP,
+    remaining_uses SMALLINT DEFAULT -1,
+    Foreign Key (user_id) REFERENCES user (id),
+    Foreign Key (group_id) REFERENCES groups (id)
+)
+
+DELIMITER /
+
+CREATE TRIGGER set_expirydate
+BEFORE INSERT ON group_invitation 
+FOR EACH ROW
+BEGIN
+    IF NEW.expire_at is NULL THEN
+        SET NEW.expire_at = NOW() + INTERVAL 5 MINUTE;
+    END IF;
+END;
+/
+
+DELIMITER;
