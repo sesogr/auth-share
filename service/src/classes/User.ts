5ac23555065b68b5dfaa1fab8a0fccf51c25b409
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-07-21 11:57:56 +0200

    feat(User): getname as a class

--- service/src/classes/User.ts
+++ service/src/classes/User.ts
@@ -1,1 +1,10 @@
-export class User {}
+import { User } from "../../ports/User.ts";
+export class UserFacade implements User {
+  private name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  getName(): string {
+    return this.name;
+  }
+}
