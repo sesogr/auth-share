c96ac0cd88f6c1dbd22e64312cf977032b08e21b
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-07-22 11:12:57 +0200

    test: inmemory database not fletched out testcases

--- service/test/MemoryDatabase.ts
+++ service/test/MemoryDatabase.ts
@@ -1,4 +1,29 @@
 import "../ports/Database.ts";
-import { Database } from "../ports/Database.ts";
+import { Database, Userinfo } from "../ports/Database.ts";
 
-export class MemoryDatabase implements Database {}
+export class MemoryDatabase implements Database {
+  private generateUsers(): Userinfo[] {
+    const Userlist: Userinfo[] = [];
+    for (let i: number = 1; i < 10; i++) {
+      Userlist.push({
+        email: `${i}@${i}.de}`,
+        name: `name${i}`,
+        passwort: `pass1234`,
+        twofaref: ``,
+      });
+    }
+    return Userlist;
+  }
+  getUserinfoList(): Userinfo[] {
+    return this.generateUsers();
+  }
+  getUserinfoByName(name: string): Userinfo {
+    const user: Userinfo | undefined = this.generateUsers().find(
+      (e) => e.name === name
+    );
+    if (user === undefined) {
+      throw new Error("user not found");
+    }
+    return user;
+  }
+}
--- service/test/test_database.ts
+++ service/test/test_database.ts
@@ -1,6 +1,12 @@
-import { assertExists } from "https://deno.land/std@0.104.0/testing/asserts.ts";
+import {
+  assertExists,
+  assertThrows,
+} from "https://deno.land/std@0.104.0/testing/asserts.ts";
+import DBManager from "../src/classes/DBManager.ts";
 
 Deno.test({
   name: "connection",
-  fn() {},
+  fn() {
+    assertThrows(() => new DBManager());
+  },
 });
