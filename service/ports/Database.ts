1061ccba6a15f1094f62375c737f55fee63cd83a
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-07-22 11:13:30 +0200

    feat: database interface with Userinfo type

--- service/ports/Database.ts
+++ service/ports/Database.ts
@@ -1,1 +1,10 @@
-export interface Database {}
+export type Userinfo = {
+  name: string;
+  passwort: string;
+  email: string;
+  twofaref: string;
+};
+export interface Database {
+  getUserinfoList(): Userinfo[];
+  getUserinfoByName([string]: string): Userinfo;
+}
