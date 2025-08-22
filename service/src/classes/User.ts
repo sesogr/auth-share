b1575296995df6c8403f7e66ea348709aac6d88a
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-08-22 11:47:07 +0200

    add methods and contructor to User

--- service/src/classes/User.ts
+++ service/src/classes/User.ts
@@ -1,23 +1,49 @@
 import { Group } from "./Group.ts";
+import { Invitation } from "./Invitation.ts";
 import { Service } from "./Service.ts";
+import { UserCredential } from "./UserCredential.ts";
 
 export class User {
-  displayName: string;
-  //I guess UserCredentials are an Object
-  credentials: UserCredentials;
-  owned: Service[];
-  callable: Service[];
-  invitedGroups: Invitation[];
-  ownedGroups: Group[];
+  private constructor(
+    private credentials: UserCredential,
+
+    private displayName: string = "",
+    private owned: Service[] = [],
+    private callable: Service[] = [],
+    private groups: Group[] = [],
+    private invitedGroups: Invitation[] = [],
+    private ownedGroups: Group[] = []
+  ) {}
 
   listOwnedServices(): Service[] {
     return this.owned;
   }
-  static authenticate(UserCredentials): User;
-  addInvitation(newInvite: Invitation);
-  listServices(): Service[];
-  changeUserCredentials(newCredentials: UserCredentials);
-  listUserGroups(): Group[];
-  listUserGroupInvitation(): Invitation[];
-  requestAuthorization(newService: Service);
+  static createUser(credentials: UserCredential, displayName: string) {
+    return new User(credentials, displayName);
+  }
+  static authenticate(credentials: UserCredential): User {
+    return new User(credentials);
+  }
+
+  addInvitation(newInvite: Invitation) {
+    this.invitedGroups.push(newInvite);
+  }
+
+  listServices(): Service[] {
+    return this.callable;
+  }
+  changeUserCredentials(newCredentials: UserCredential) {}
+
+  listOwnedGroups(): Group[] {
+    return this.ownedGroups;
+  }
+
+  listUserGroups(): Group[] {
+    return this.groups;
+  }
+
+  listUserGroupInvitation(): Invitation[] {
+    return this.invitedGroups;
+  }
+  requestAuthorization(newService: Service) {}
 }
