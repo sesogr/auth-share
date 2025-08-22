0e6fe15897651455e995918dab5aa0823d3dfc18
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-08-22 11:47:43 +0200

    add methods and constructor for Group

--- service/src/classes/Group.ts
+++ service/src/classes/Group.ts
@@ -1,13 +1,31 @@
+import { Invitation } from "./Invitation.ts";
 import { User } from "./User.ts";
 
-export class Group{
-    groupname: string;
-
-    users: User[];
-    sentUserInvitations: Invitation[];
-
-    listServiceInvitation(): Invitation[];
-    static createUserGroup(: groupname,): Group;
-    sendInvitation(: User);
-    addServiceInvitation(: Invitation);
+export class Group {
+  private constructor(
+    private groupname: string,
+    private owner: User,
+    private users: User[] = [],
+    private sentUserInvitations: Invitation[] = [],
+    private serviceInvitations: Invitation[] = []
+  ) {}
+  listServiceInvitation(): Invitation[] {
+    return this.serviceInvitations;
+  }
+  listSentUserInvitation(): Invitation[] {
+    return this.sentUserInvitations;
+  }
+  static createUserGroup(groupname: string, owner: User): Group {
+    return new Group(groupname, owner);
+  }
+  sendInvitation(receiver: User) {
+    const invite: Invitation = new Invitation(
+      this.groupname,
+      this.owner.getDisplayName()
+    );
+    receiver.addInvitation(invite);
+  }
+  addServiceInvitation(newServiceInvite: Invitation) {
+    this.serviceInvitations.push(newServiceInvite);
+  }
 }
