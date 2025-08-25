622e6c7fd7f449443416260b75a9b212dd007860
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-08-25 14:02:32 +0200

    adds senderRefence and ReceiverReferene to Invitation

--- service/src/classes/Invitation.ts
+++ service/src/classes/Invitation.ts
@@ -1,19 +1,30 @@
 import { ValueClass } from "./ValueClass.ts";
 import { Displayable } from "../interfaces/Displayable.ts";
-export class Invitation<T extends Displayable> extends ValueClass {
+import { User } from "./User.ts";
+export class Invitation<
+  T extends Displayable,
+  U extends Displayable
+> extends ValueClass {
   constructor(
-    private readonly sendername: string,
-    private readonly reference: T
+    private readonly senderReference: User,
+    private readonly objReference: T,
+    private readonly receiverRefence: U
   ) {
     super();
   }
   toString() {
-    return `${this.sendername}:${this.reference.getDisplayName()}`;
+    return `${this.senderReference.getDisplayName()}:${this.objReference.getDisplayName()}:${this.receiverRefence.getDisplayName()}`;
   }
-  equals(that: Invitation<T>) {
+  equals(that: Invitation<T, U>) {
     return this.toString() === that.toString();
   }
-  getReference(): T {
-    return this.reference;
+  getObjReference(): T {
+    return this.objReference;
+  }
+  getSenderReference(): User {
+    return this.senderReference;
+  }
+  getReceiverReference(): U {
+    return this.receiverRefence;
   }
 }
