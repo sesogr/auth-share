6d8d61f63a37150f6699f6b4eff2ceff810c6b8b
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-08-28 14:57:56 +0200

    add new tests for class group

--- service/test/Group_test.ts
+++ service/test/Group_test.ts
@@ -1,8 +1,12 @@
-import { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.224.0/assert/mod.ts";
 import { Group } from "../src/classes/Group.ts";
 import { User } from "../src/classes/User.ts";
 import { UserCredential } from "../src/classes/UserCredential.ts";
 import { assertArrayIncludes } from "https://deno.land/std@0.224.0/assert/assert_array_includes.ts";
+import { Invitation } from "../src/classes/Invitation.ts";
 
 const userCredential = new UserCredential("Hans Meiser", "abcdef");
 const user = User.createUser(userCredential, "");
@@ -50,3 +54,45 @@ Deno.test("test2 whether the list serviceInvitations is empty", () => {
   console.log("The lenght of the List is ", length, " and should be 0, right?");
   assertEquals(length, 0);
 });
+
+Deno.test(
+  "test for method sendInvitation - will the invitation put correctly in the lists?",
+  () => {
+    const group = createTestGroup();
+    const testReciever = new UserCredential("Don Receiver", "qwertz");
+    const testUser = User.createUser(testReciever, "Don Receiver");
+    //const sender = group.getOwner();
+    group.sendInvitation(testUser);
+
+    const listSentInvitation = group.listSentInvitation();
+    assertEquals(listSentInvitation, testUser.listUserGroupInvitation());
+  }
+);
+
+Deno.test(
+  "test2 for method sendInvitation with a testInvitation - will the invitation put correctly in the lists?",
+  () => {
+    const group: Group = createTestGroup();
+    const testUserCredentials: UserCredential = new UserCredential(
+      "Don Receiver",
+      "qwertz"
+    );
+    const testReceiver: User = User.createUser(
+      testUserCredentials,
+      "Don Receiver"
+    );
+    const testInvitation: Invitation<Group, User> = new Invitation(
+      user,
+      group,
+      testReceiver
+    );
+    //const sender = group.getOwner();
+    group.sendInvitation(testReceiver);
+
+    const listSentInvitation = group.listSentInvitation();
+    const listUserGroupInvitation = testReceiver.listUserGroupInvitation();
+
+    assertArrayIncludes(listSentInvitation, [testInvitation]);
+    assertArrayIncludes(listUserGroupInvitation, [testInvitation]);
+  }
+);
