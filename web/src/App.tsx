500115c48467e3c47926abb088d319d588c2a6d4
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-09-03 16:47:55 +0200

    add the first frontend solution for User List

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, type JSX } from "react";
+import type { ConvertedUser } from "./types/types.ts";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(): JSX.Element {
+  const [users, setUsers] = useState<ConvertedUser[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  fetch("http://localhost:8000/user") // Port/Host anpassen
+    .then((res): Promise<ConvertedUser[]> => {
+      if (!res.ok) throw new Error("Netzwerkfehler");
+      return res.json();
+    })
+    .then((data: ConvertedUser[]) => setUsers(data))
+    .catch((err) => setError(err.message));
+
+  if (error) return <div>Fehler: {error}</div>;
+  if (!users) return <div>Lade...</div>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map((u) => (
+          <li>{u.displayname}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-export default App
+export default App;
