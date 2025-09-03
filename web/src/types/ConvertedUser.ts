2c23fc43c01d047b4400720f7012745f4b2f2871
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-09-03 16:49:19 +0200

    copy backend types

---
+++ web/src/types/ConvertedUser.ts
@@ -0,0 +1,15 @@
+import { UserCredential } from "../classes/UserCredential.ts";
+
+export type ConvertedUser = {
+  credentials: UserCredential;
+  displayname: string;
+} & {
+  [k in UserListProperties]: string[];
+};
+
+type UserListProperties =
+  | "owned"
+  | "callable"
+  | "groups"
+  | "userGroupInvitations"
+  | "ownedGroups";
---
+++ web/src/types/types.ts
@@ -0,0 +1,5 @@
+import { ConvertedGroup } from "./ConvertedGroup.ts";
+import { ConvertedService } from "./ConvertedService.ts";
+import { ConvertedUser } from "./ConvertedUser.ts";
+
+export type { ConvertedGroup, ConvertedService, ConvertedUser };
