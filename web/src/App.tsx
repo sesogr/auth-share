e539361918229d6ef40c9418e7df79f1115a8c2d
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-09-05 14:58:44 +0200

    add the User Route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,12 +2,14 @@ import { type JSX } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home.tsx";
 import Service from "./Service.tsx";
+import User from "./User.tsx";
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
         <Route path="/:serviceName" element={<Home />} />
         <Route path="*" element={<div>Missing Page!!</div>} />
         <Route path="/owned" element={<Service />} />
