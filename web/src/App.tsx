fae8d719001004347e54f5392bf4e8d984a97264
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-09-08 13:24:51 +0200

    add a new import for the Navbar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,18 +3,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home.tsx";
 import Service from "./Service.tsx";
 import User from "./User.tsx";
+import Navbar from "./Navbar.tsx";
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user" element={<User />} />
         <Route path="/:serviceName" element={<Home />} />
-        <Route path="*" element={<div>Missing Page!!</div>} />
         <Route path="/owned" element={<Service />} />
+        <Route path="*" element={<div>Missing Page!!</div>} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
