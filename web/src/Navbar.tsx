80fc144e0e66d3d5bf3e9cf450383d4203b48a19
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-09-08 13:30:14 +0200

    add a new component called Navbar to the project

---
+++ web/src/Navbar.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  const linkStyle = ({ isActive }: { isActive: boolean }) => ({
+    marginRight: 12,
+    textDecoration: "none",
+    color: isActive ? "DarkSlateGrey" : "black",
+    fontWeight: isActive ? "600" : "400",
+  });
+
+  return (
+    <nav style={{ padding: 12, borderBottom: "1px solid #ffffffff" }}>
+      <NavLink to="/" style={linkStyle}>
+        Home
+      </NavLink>
+      <NavLink to="/user" style={linkStyle}>
+        User
+      </NavLink>
+    </nav>
+  );
+}
+
+export default Navbar;
