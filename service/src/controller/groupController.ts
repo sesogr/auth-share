8d248f40f690d4b3904baffee9bf24c2dddc5361
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-09-17 16:06:07 +0200

    remove old code

--- service/src/controller/groupController.ts
+++ service/src/controller/groupController.ts
@@ -1,8 +1,6 @@
 import { Context } from "@hono/hono";
-import { testGroup } from "../main.ts";
-import { ConvertedGroup } from "../types/ConvertedGroup.ts";
 
 export const groupController = (c: Context) => {
-  const convertedGroupList: ConvertedGroup[] = testGroup.map((e) => e.toJson());
-  return c.json(convertedGroupList);
+  //  const convertedGroupList: ConvertedGroup[] = testGroup.map((e) => e.toJson());
+  return c; //.json(convertedGroupList);
 };
--- service/src/controller/userController.ts
+++ service/src/controller/userController.ts
@@ -1,8 +1,6 @@
 import { Context } from "@hono/hono";
-import { ConvertedUser } from "../types/ConvertedUser.ts";
-import { userList } from "../main.ts";
 
 export const userController = (c: Context) => {
-  const convertedList: ConvertedUser[] = userList.map((e) => e.toJson());
-  return c.json(convertedList);
+  //const convertedList: ConvertedUser[] = userList.map((e) => e.toJson());
+  return c; //.json();//convertedList);
 };
