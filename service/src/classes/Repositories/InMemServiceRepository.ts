87f6c1aee07e3478ba42a2e4c44f987c67e5f376
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-09-22 14:39:12 +0200

    remove wrong methods

--- service/src/classes/Repositories/InMemServiceRepository.ts
+++ service/src/classes/Repositories/InMemServiceRepository.ts
@@ -29,34 +29,21 @@ export class InMemServiceRepository extends InMemoryRepository<Service>
     super(serviceList);
   }
   listAuthorizedUsers(serviceId: string): string[] {
-    return this.allowedUser.filter((e) => e.serviceId === serviceId).map((
-      e,
-    ) => e.userId);
+    throw new Error("Method not implemented.");
   }
   listAuthorizedGroups(serviceId: string): string[] {
-    return this.allowedGroups.filter((e) => e.serviceId === serviceId).map((
-      e,
-    ) => e.groupId);
+    throw new Error("Method not implemented.");
   }
   listOwners(serviceId: string): string[] {
-    return this.allowedUser.filter((e) => e.serviceId === serviceId).filter(
-      (e) => e.isOwner,
-    ).map((
-      e,
-    ) => e.userId);
+    throw new Error("Method not implemented.");
   }
   createService(
     ownerId: string,
     credentials: ServiceCredential,
     serviceName: string,
   ): void {
-    const service = new Service(credentials, serviceName);
-    this._allowedUser.push(
-      new AllowedUserServiceMap(ownerId, service.getId(), true),
-    );
-  }
-  giveAuthorizationToUser(serviceId: string, userId: string): void {
-    this._allowedUser.push(new AllowedUserServiceMap(userId, serviceId));
+    console.log(ownerId, credentials, serviceName);
+    throw new Error("Method not implemented.");
   }
   override removeById(serviceId: string): void {
     try {
@@ -70,23 +57,5 @@ export class InMemServiceRepository extends InMemoryRepository<Service>
     this._allowedGroups = this.allowedGroups.filter((e) =>
       e.serviceId !== serviceId
     );
-  }
-  toJsonString(serviceId: string): string {
-    return JSON.stringify(this.convertToSerializeableObj(serviceId));
-  }
-  private convertToSerializeableObj(serviceId: string): ConvertedService {
-    const service: Service = this.findById(serviceId);
-    return {
-      serviceName: service.getDisplayName(),
-      credentials: service.credentials,
-      groups: this.listAuthorizedGroups(serviceId),
-      users: this.listAuthorizedUsers(serviceId),
-      owners: this.listOwners(serviceId),
-      sentInvitations: service.sentInvitations.map((e) => e.toString()),
-    };
-  }
-
-  toJson(serviceId: string) {
-    return this.convertToSerializeableObj(serviceId);
   }
 }
