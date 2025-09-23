0a560a34eace95c77bf6f3ef5d5d09f466bc7d5c
vhusername123 <17260044+mauer01@users.noreply.github.com>
2025-09-23 10:57:45 +0200

    implement all methods todo error throwers

--- service/src/classes/Repositories/InMemServiceRepository.ts
+++ service/src/classes/Repositories/InMemServiceRepository.ts
@@ -1,9 +1,10 @@
 import { FakeObjectGen } from "../../FakeObjectGen.ts";
 import { ServiceRepository } from "../../interfaceTypes/ServiceRepository.ts";
-import { ConvertedService } from "../../types/ConvertedService.ts";
 
-import { AllowedGroupMap } from "../AllowedGroupMap.ts";
+import { AllowedGroupServiceMap } from "../AllowedGroupServiceMap.ts";
 import { AllowedUserServiceMap } from "../AllowedUserServiceMap.ts";
+import { Group } from "../Group.ts";
+import { Invitation } from "../Invitation.ts";
 
 import { Service } from "../Service.ts";
 import { ServiceCredential } from "../ServiceCredential.ts";
@@ -12,14 +13,62 @@ import { InMemoryRepository } from "./InMemoryRepository.ts";
 
 export class InMemServiceRepository extends InMemoryRepository<Service>
   implements ServiceRepository {
-  private _allowedUser: AllowedUserServiceMap[] = [];
-  public get allowedUser(): AllowedUserServiceMap[] {
-    return [...this._allowedUser];
+  override save(service: Service): void {
+    let serviceIndex = this.inMemList.findIndex((e) =>
+      service.getId() === e.getId()
+    );
+    if (!serviceIndex) {
+      this.add(service);
+      serviceIndex = this.inMemList.length - 1;
+    }
+    this.inMemList[serviceIndex] = service;
+    this.updateInvites(service.sentInvitations);
+    this.updateAllowedUsers(service.authorizedUsers);
+    this.updateAllowedGroups(service.authorizedGroups);
   }
-  private _allowedGroups: AllowedGroupMap[] = [];
-  public get allowedGroups(): AllowedGroupMap[] {
-    return [...this._allowedGroups];
+  private updateInvites(invites: Invitation<Service, Group>[]) {
+    const missingInvites = invites.filter((e) =>
+      !this.invitations.some((f) => e.equals(f))
+    );
+    this.invitations.push(...missingInvites);
+    const deletedInvites = this.invitations.filter((e) =>
+      invites.some((f) => e.equals(f))
+    );
+    this.invitations = this.invitations.filter((e) =>
+      !deletedInvites.some((f) => e.equals(f))
+    );
   }
+  private updateAllowedUsers(authorizedUsers: AllowedUserServiceMap[]) {
+    const missingAllowedUsers = authorizedUsers.filter((e) =>
+      !this.allowedUser.some((f) => e.equals(f))
+    );
+    this.allowedUser.push(...missingAllowedUsers);
+    const unauthorizedUsers = this.allowedUser.filter((e) =>
+      authorizedUsers.some((f) => e.equals(f))
+    );
+    this.allowedUser = this.allowedUser.filter((e) =>
+      !unauthorizedUsers.some((f) => e.equals(f))
+    );
+  }
+  private updateAllowedGroups(authorizedGroups: AllowedGroupServiceMap[]) {
+    const missingAllowedGroups = authorizedGroups.filter((e) =>
+      !this.allowedGroups.some((f) => e.equals(f))
+    );
+    this.allowedGroups.push(...missingAllowedGroups);
+    const unauthorizedUsers = this.allowedGroups.filter((e) =>
+      authorizedGroups.some((f) => e.equals(f))
+    );
+    this.allowedGroups = this.allowedGroups.filter((e) =>
+      !unauthorizedUsers.some((f) => e.equals(f))
+    );
+  }
+  override hydrate(_item: Service): Service {
+    throw new Error("unimplemented");
+  }
+  private allowedUser: AllowedUserServiceMap[] = [];
+
+  private allowedGroups: AllowedGroupServiceMap[] = [];
+  private invitations: Invitation<Service, Group>[] = [];
   constructor(user: User) {
     const serviceList: Service[] = [];
     for (let i = 0; i < 10; i++) {
@@ -28,22 +77,29 @@ export class InMemServiceRepository extends InMemoryRepository<Service>
     }
     super(serviceList);
   }
-  listAuthorizedUsers(serviceId: string): string[] {
-    throw new Error("Method not implemented.");
-  }
-  listAuthorizedGroups(serviceId: string): string[] {
-    throw new Error("Method not implemented.");
-  }
-  listOwners(serviceId: string): string[] {
-    throw new Error("Method not implemented.");
-  }
   createService(
-    ownerId: string,
-    credentials: ServiceCredential,
-    serviceName: string,
-  ): void {
-    console.log(ownerId, credentials, serviceName);
+    _ownerId: string,
+    _credentials: ServiceCredential,
+    _serviceName: string,
+  ): Service {
     throw new Error("Method not implemented.");
+  }
+  findOwnedByUserId(userId: string): Service[] {
+    return this.allowedUser.filter((currMap) =>
+      (currMap.userId === userId) && currMap.isOwner
+    ).map((currMap) => this.findById(currMap.serviceId));
+  }
+  findAuthorizedForId(_Id: string): Service[] {
+    throw new Error("Method not implemented.");
+  }
+  viewAllowedUser(): AllowedUserServiceMap[] {
+    return [...this.allowedUser];
+  }
+  viewAllowedGroups(): AllowedGroupServiceMap[] {
+    return [...this.allowedGroups];
+  }
+  viewInvitedGroups(): Invitation<Service, Group>[] {
+    return [...this.invitations];
   }
   override removeById(serviceId: string): void {
     try {
@@ -51,10 +107,10 @@ export class InMemServiceRepository extends InMemoryRepository<Service>
     } catch (e) {
       throw Error(`service: ${serviceId} not removed, ${e}`);
     }
-    this._allowedUser = this.allowedUser.filter((e) =>
+    this.allowedUser = this.allowedUser.filter((e) =>
       e.serviceId !== serviceId
     );
-    this._allowedGroups = this.allowedGroups.filter((e) =>
+    this.allowedGroups = this.allowedGroups.filter((e) =>
       e.serviceId !== serviceId
     );
   }
