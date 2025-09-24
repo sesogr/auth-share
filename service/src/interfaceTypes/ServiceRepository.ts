7e4d6357f9b03481fb2d1e50d550b7a7b5c0b79b
vhusername123 <17260044+mauer01@users.noreply.github.com>
2025-09-24 10:35:54 +0200

    repositories dont have factory methods anymore

--- service/src/interfaceTypes/ServiceRepository.ts
+++ service/src/interfaceTypes/ServiceRepository.ts
@@ -1,5 +1,4 @@
 import { Service } from "../classes/Service.ts";
-import { ServiceCredential } from "../classes/ServiceCredential.ts";
 import type { Repository } from "./Repository.ts";
 import { ServiceRepositoryView } from "./ServiceRepositoryView.ts";
 
@@ -7,11 +6,6 @@ export type ServiceRepository =
   & Repository<Service>
   & ServiceRepositoryView
   & {
-    createService(
-      ownerId: string,
-      credentials: ServiceCredential,
-      serviceName: string,
-    ): Service;
     findOwnedByUserId(userId: string): Service[];
     findAuthorizedForId(Id: string): Service[];
     fillWithMockData(userIdList: string[]): void;
