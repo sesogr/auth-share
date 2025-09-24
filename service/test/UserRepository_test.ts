93dde10c49905af2319e9696f8bb3f0c196c1a4b
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-09-24 15:00:50 +0200

    add all fillWithMockData methods for user,group and service,and add more tests

--- service/test/UserRepository_test.ts
+++ service/test/UserRepository_test.ts
@@ -1,8 +1,4 @@
-//von der user repo ein user findet/bekommt -->
-//servicerepo test
-//test für findbyid => alle properties nicht logisch falsch sind
-
-import { assertEquals, assertGreater } from "@std/assert";
+import { assertEquals, assertThrows } from "@std/assert";
 import { InMemGroupRepository } from "../src/classes/Repositories/InMemGroupRepository.ts";
 import { InMemServiceRepository } from "../src/classes/Repositories/InMemServiceRepository.ts";
 import { InMemUserRepository } from "../src/classes/Repositories/InMemUserRepository.ts";
@@ -10,6 +6,9 @@ import { GroupRepository } from "../src/interfaceTypes/GroupRepository.ts";
 import { ServiceRepository } from "../src/interfaceTypes/ServiceRepository.ts";
 import { UserRepository } from "../src/interfaceTypes/UserRepository.ts";
 import { User } from "../src/classes/User.ts";
+import { assertArrayIncludes } from "https://deno.land/std@0.224.0/assert/assert_array_includes.ts";
+import { FakeObjectGen } from "../src/FakeObjectGen.ts";
+import { NotFoundError } from "../src/errors/NotFoundError.ts";
 
 const serviceRepository: ServiceRepository = new InMemServiceRepository();
 const groupRepository: GroupRepository = new InMemGroupRepository(
@@ -19,29 +18,62 @@ const userRepository: UserRepository = new InMemUserRepository(
   serviceRepository,
   groupRepository,
 );
+function fillWithMockGroupData(userIdList: string[]): void {
+  for (let i = 0; i < 5; i++) {
+    const rand = Math.round(Math.random() * userIdList.length);
+    const fakeGroup = FakeObjectGen.createFakeGroup(
+      undefined,
+      userIdList[rand],
+    );
+    groupRepository.save(fakeGroup);
+  }
+}
+function fillWithMockServiceData(userIdList: string[]): void {
+  for (let i = 0; i < 10; i++) {
+    const rand = Math.round(Math.random() * userIdList.length);
+    const fakeService = FakeObjectGen.createFakeService(userIdList[rand]);
+    serviceRepository.save(fakeService);
+  }
+}
 
-userRepository.fillWithMockData();
-const mockUserList: string[] = userRepository.findAll().map((e) => e.getId());
-serviceRepository.fillWithMockData(mockUserList);
+function fillWithMockUserData(): User[] {
+  const fakeUserList: User[] = [];
+  for (let i = 0; i < 10; i++) {
+    const fakeUser = FakeObjectGen.createFakeUser();
+    fakeUserList.push(fakeUser);
+    userRepository.save(fakeUser);
+  }
+  return fakeUserList;
+}
+const fakeUserList: User[] = fillWithMockUserData();
 
-groupRepository.fillWithMockData(mockUserList);
+const mockUserIdList: string[] = fakeUserList.map((e) => e.getId());
+fillWithMockServiceData(mockUserIdList);
+fillWithMockGroupData(mockUserIdList);
 
 Deno.test("Test for method findById()", () => {
-  const userList: User[] = userRepository.findAll();
-  assertGreater(userList.length, 0);
-
-  const userId: string = userList[0].getId();
+  const userId: string = mockUserIdList[0];
   const user = userRepository.findById(userId);
 
   assertEquals(user.getId(), userId);
 });
 
 Deno.test("Test for method findByName()", () => {
-  const userList: User[] = userRepository.findAll();
-  assertGreater(userList.length, 3);
-
-  const userName: string = userList[3].getDisplayName();
+  const userName: string = fakeUserList[3].getDisplayName();
   const toCheck: string = userRepository.findByName(userName).getDisplayName();
 
   assertEquals(toCheck, userName);
+});
+
+Deno.test("Test for method findAll()", () => {
+  const userList: User[] = userRepository.findAll();
+  assertArrayIncludes(userList, fakeUserList);
+});
+
+Deno.test("Test for method removeById", () => {
+  const deletedId = mockUserIdList[2];
+  userRepository.removeById(deletedId);
+  assertThrows(() => {
+    userRepository.findById(deletedId);
+  }, NotFoundError);
 });
