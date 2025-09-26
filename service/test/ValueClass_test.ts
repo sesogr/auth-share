5478ed0068a43a84fc504b4ec24d25f168d96393
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-09-26 11:48:37 +0200

    forgot

--- service/test/ValueClass_test.ts
+++ service/test/ValueClass_test.ts
@@ -2,7 +2,7 @@ import { assertFalse } from "@std/assert";
 import { ServiceCredential } from "../src/classes/ServiceCredential.ts";
 import { UserCredential } from "../src/classes/UserCredential.ts";
 
-Deno.test("testing valueclass", async (t) => {
+Deno.test("Value Class", async (t) => {
   const serviceCred1 = new ServiceCredential("hallo", "hallo2");
   const serviceCred2 = new ServiceCredential("hallo", "hallo2");
   await t.step("Equality", () => {
