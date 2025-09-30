9f916e1a8aa01637cfef9c7dfaeac4a02fe51c27
vhusername123 <17260044+mauer01@users.noreply.github.com>
2025-09-30 12:23:27 +0200

    add get accessors for invitation

--- service/src/classes/Invitation.ts
+++ service/src/classes/Invitation.ts
@@ -5,6 +5,30 @@ export class Invitation extends ValueClass {
   public get receiverReference(): ShortEntity {
     return this._receiverReference;
   }
+
+  public get reveicername(): string {
+    return this._receiverReference.displayname;
+  }
+
+  public get receiverId(): string {
+    return this._receiverReference.id;
+  }
+
+  public get objname(): string {
+    return this._objReference.displayname;
+  }
+  public get objId(): string {
+    return this._objReference.id;
+  }
+
+  public get senderId(): string {
+    return this._senderReference.id;
+  }
+
+  public get sendername(): string {
+    return this._senderReference.displayname;
+  }
+
   public get objReference(): ShortEntity {
     return this._objReference;
   }
