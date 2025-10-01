70d050cd74d91f652f0724f6b8d79a38f3ee0083
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-10-01 16:17:18 +0200

    get testuser and prepare for output

--- service/src/controller/userController.ts
+++ service/src/controller/userController.ts
@@ -1,15 +1,40 @@
 import { Context } from "@hono/hono";
 import { UserRepository } from "../interfaceTypes/UserRepository.ts";
-import { ConvertedUser } from "../types/ConvertedUser.ts";
 import { User } from "../classes/User.ts";
 import { UserCredential } from "../classes/UserCredential.ts";
+import { User as DbUser } from "../classes/Repositories/DenoDB/Models/DbUser.ts";
+import { UserCredential as DbUserCredential } from "../classes/Repositories/DenoDB/Models/DbUser.ts";
 
-export const userController = (userRepository: UserRepository) => ({
-  read: (c: Context) => {
-    const user: ConvertedUser[] = userRepository.findAll().map((e) =>
-      e.toJson()
+export const userController = (
+  userRepository: UserRepository,
+) => ({
+  read: async (c: Context) => {
+    const user = await DbUser.where(
+      "id",
+      "testID1",
+    ).get();
+    if (!Array.isArray(user)) {
+      throw new Error("User not found");
+    }
+    //const convertedUser: ConvertedUser = new User(user.credentials, user.displayname, user.id);
+    console.log(user);
+    const credentials = await DbUserCredential.where("user_id", "testID1")
+      .get();
+    if (!Array.isArray(credentials)) {
+      throw new Error("Credentials not found");
+    }
+    return c.json(
+      user.map((e) =>
+        new User(
+          new UserCredential(
+            credentials[0]["_username"] + "",
+            credentials[0]["_password"] + "",
+          ),
+          e.displayname + "",
+          e.id + "",
+        ).toJson()
+      ),
     );
-    return c.json(user);
   },
   changePassword: async (c: Context) => {
     const requestData = await c.req.json();
