6b6b0f11013c8eccdb0e0d9686cfb8966a935f79
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-10-06 13:29:58 +0200

    add Database Connection and a db.link between DbUser->DbUserCredentials, implementing the add()

--- service/src/classes/Repositories/DenoDB/DbUserRepository.ts
+++ service/src/classes/Repositories/DenoDB/DbUserRepository.ts
@@ -1,44 +1,49 @@
-// import { UserRepository } from "../../../interfaceTypes/UserRepository.ts";
-// import { User } from "../../User.ts";
-// import { UserCredential } from "../../UserCredential.ts";
-// import { DbUser } from "./Models/DbUser.ts";
-// import { DbUserCredential } from "./Models/DbUserCredentials.ts";
+import { UserRepository } from "../../../interfaceTypes/UserRepository.ts";
+import { User } from "../../User.ts";
+import { DbUser } from "./Models/DbUser.ts";
+import { DbUserCredential } from "./Models/DbUserCredentials.ts";
+import { Database, MySQLConnector } from "@denodb";
 
-// export class DbUserRepository implements UserRepository {
-//   constructor() {}
-//   async findById(id: string): User {
-//     return this.hydrate(await DbUser.find(id));
-//   }
-//   findByName(name: string): User {
-//     throw new Error("Method not implemented.");
-//   }
-//   findAll(): User[] {
-//     throw new Error("Method not implemented.");
-//   }
-//   add(item: User): void {
-//     throw new Error("Method not implemented.");
-//   }
-//   removeById(id: string): void {
-//     throw new Error("Method not implemented.");
-//   }
-//   save(item: User): void {
-//     throw new Error("Method not implemented.");
-//   }
-//   hydrate(item: DbUser): User {
-//     const id = item.id;
-//     const credentials = new UserCredential(
-//       item.credentials.username,
-//       item.credentials.password,
-//     );
-//     )
-//     const user: User = new User(
-//       credentials,
-//       displayname,
-//       id,
-//       serviceList,
-//       invitations,
-//       joinedGroups,
-//     );
-//     return user;
-//   }
-// }
+export class DbUserRepository implements UserRepository {
+  private readonly connector = new MySQLConnector({
+    database: Deno.env.get("DB_NAME")!,
+    host: Deno.env.get("DB_HOST")!,
+    username: Deno.env.get("DB_USER")!,
+    password: Deno.env.get("DB_PASSWORD")!,
+  });
+  constructor() {
+    const db = new Database(this.connector);
+    db.link([DbUser, DbUserCredential]);
+  }
+  findById(_id: string): User {
+    throw new Error("Method not implemented.");
+  }
+  findByName(_name: string): User {
+    throw new Error("Method not implemented.");
+  }
+  findAll(): User[] {
+    throw new Error("Method not implemented.");
+  }
+  add(item: User): void {
+    DbUser.create({
+      displayname: item.getDisplayName(),
+      id: item.getId(),
+    });
+    DbUserCredential.create({
+      dbuser_id: item.getId(),
+      username: item.getCredentials().username,
+      password: item.getCredentials().password,
+    });
+  }
+  removeById(_id: string): void {
+    throw new Error("Method not implemented.");
+  }
+  async save(item: User) {
+    if (!(await DbUser.find(item.getId()))) {
+      this.add(item);
+    }
+  }
+  hydrate(_item: DbUser): User {
+    throw new Error("Method not implemented.");
+  }
+}
