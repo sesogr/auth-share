34c02b87f28fef23da0d56c16c06c1beebb6157a
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-10-07 11:40:06 +0200

    refactor code -> using userRepository

--- service/src/controller/userController.ts
+++ service/src/controller/userController.ts
@@ -2,46 +2,22 @@ import { Context } from "@hono/hono";
 import { UserRepository } from "../interfaceTypes/UserRepository.ts";
 import { User } from "../classes/User.ts";
 import { UserCredential } from "../classes/UserCredential.ts";
-import { DbUser } from "../classes/Repositories/DenoDB/Models/DbUser.ts";
-import { DbUserCredential } from "../classes/Repositories/DenoDB/Models/DbUserCredentials.ts";
 import { ConvertedUser } from "../types/types.ts";
 
 export const createUserController = (
   userRepository: UserRepository,
 ) => ({
   read: async (c: Context) => {
-    const user = await DbUser.where(
-      "id",
-      "testID1",
-    ).get();
-    if (!Array.isArray(user)) {
-      throw new Error("User not found");
-    }
-    //const convertedUser: ConvertedUser = new User(user.credentials, user.displayname, user.id);
-    console.log(user);
-    const credentials = await DbUserCredential.where("user_id", "testID1")
-      .get();
-    if (!Array.isArray(credentials)) {
-      throw new Error("Credentials not found");
-    }
+    const user = await userRepository.findById("testID1");
     return c.json(
-      user.map((e) =>
-        new User(
-          new UserCredential(
-            credentials[0]["_username"] + "",
-            credentials[0]["_password"] + "",
-          ),
-          e.displayname + "",
-          e.id + "",
-        ).toJson()
-      ),
+      user.toJson(),
     );
   },
   changePassword: async (c: Context) => {
     const requestData = await c.req.json();
     const newPassword: string = requestData.password;
     //TODO we need the loggedin User here!!
-    const myself: User = userRepository.findAll()[0];
+    const myself: User = (await userRepository.findAll())[0];
     myself.changeUserCredentials(
       new UserCredential(myself.getCredentials().username, newPassword),
     );
