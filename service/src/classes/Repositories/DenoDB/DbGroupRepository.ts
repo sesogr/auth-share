903b6b188aa9baeb31783c4020a326145530a34d
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-10-09 10:45:55 +0200

    update hydration method and add static methods in DbGroup

--- service/src/classes/Repositories/DenoDB/DbGroupRepository.ts
+++ service/src/classes/Repositories/DenoDB/DbGroupRepository.ts
@@ -4,36 +4,92 @@ import { DbGroup } from "./Models/DbGroup.ts";
 import { DbUserGroup } from "./Models/DbUserGroup.ts";
 import { DbIdDisplayname } from "./Models/DbIdDisplayname.ts";
 import { RuntimeError } from "../../../errors/RuntimeError.ts";
+import { IdNameMap } from "../../IdNameMap.ts";
+import { AllowedGroupServiceMap } from "../../AllowedGroupServiceMap.ts";
+import { Invitation } from "../../Invitation.ts";
+import { AllowedUserGroupMap } from "../../AllowedUserGroupMap.ts";
 
 export class DbGroupRepository implements GroupRepository {
   async findByName(name: string): Promise<Group> {
     const searchedName = await DbGroup.where("groupname", name).first();
     return this.hydrate(searchedName.id?.toString() ?? "");
   }
-  hydrate(_group_id: string): Promise<Group> {
-    // const groupData = await DbGroup.where("id", group_id).first();
-    // const groupname = groupData.groupname?.toString() ?? "";
-    // const groupOwnerData = await DbIdDisplayname.where(
-    //   "id",
-    //   groupData.owner?.toString() ?? "",
-    // ).first();
-    // const owner = new IdNameMap(
-    //   groupOwnerData.id?.toString() ?? "",
-    //   groupOwnerData.displayname?.toString() ?? "",
-    // );
-    // //
-    // const serviceList = await DbGroup.where();
-    // const group: Group = new Group(
-    //   groupname,
-    //   owner,
-    //   group_id,
-    //   serviceList,
-    //   sentInvitations,
-    //   serviceInvitations,
-    //   allowedUser,
-    // );
-    // return group;
-    throw new RuntimeError();
+  async hydrate(searchedId: string): Promise<Group> {
+    const groupData = await DbGroup.where("id", searchedId).first();
+    const groupname = groupData.groupname?.toString() ?? "";
+    const groupOwnerData = await DbIdDisplayname.where(
+      "id",
+      groupData.owner?.toString() ?? "",
+    ).first();
+    const owner = new IdNameMap(
+      groupOwnerData.id?.toString() ?? "",
+      groupOwnerData.displayname?.toString() ?? "",
+    );
+    //
+    const groupServiceData = await DbGroup.where("id", searchedId)
+      .knownServices();
+
+    const serviceList: AllowedGroupServiceMap[] = await Promise.all(
+      groupServiceData.map(
+        async (e) => {
+          if (!e.service_id) {
+            throw new Error("Expected for typesafety");
+          }
+          const serviceModel = await DbIdDisplayname.where(
+            "id",
+            e.service_id.toString(),
+          )
+            .select(
+              "displayname",
+            ).first();
+          const serviceName = serviceModel.displayname;
+          return new AllowedGroupServiceMap(
+            new IdNameMap(searchedId, groupname.toString() ?? ""),
+            new IdNameMap(
+              e.service_id.toString(),
+              serviceName?.toString() ?? "",
+            ),
+          );
+        },
+      ),
+    );
+    //SQL Query
+    const userGroupData = await DbGroup.where("dbuser_id", searchedId)
+      .authorizedUsers();
+    const allowedUser: AllowedUserGroupMap[] = await Promise.all(
+      userGroupData.map(
+        async (e) => {
+          const userShortModel = await DbIdDisplayname.where(
+            "id",
+            e.user_id?.toString() ?? "",
+          )
+            .select(
+              "displayname",
+            ).first();
+          const userName = userShortModel.displayname;
+          return new AllowedUserGroupMap(
+            new IdNameMap(searchedId, groupname?.toString() ?? ""),
+            new IdNameMap(
+              e.user_id?.toString() ?? "",
+              userName?.toString() ?? "",
+            ),
+            e.is_owner?.valueOf() as boolean ?? false,
+          );
+        },
+      ),
+    );
+    const sentInvitations: Invitation[] = [];
+    const serviceInvitations: Invitation[] = [];
+    const group: Group = new Group(
+      groupname,
+      owner,
+      searchedId,
+      serviceList,
+      sentInvitations,
+      serviceInvitations,
+      allowedUser,
+    );
+    return group;
   }
   async findOwnedByUserId(userId: string): Promise<Group[]> {
     const searchedList = await DbUserGroup.where({
--- service/src/classes/Repositories/DenoDB/Models/DbGroup.ts
+++ service/src/classes/Repositories/DenoDB/Models/DbGroup.ts
@@ -1,5 +1,7 @@
 import { DataTypes, Model } from "@denodb";
 import { DbIdDisplayname } from "./DbIdDisplayname.ts";
+import { DbGroupService } from "./DbGroupService.ts";
+import { DbUserGroup } from "./DbUserGroup.ts";
 import { DbInvitation } from "./DbInvitation.ts";
 export class DbGroup extends Model {
   static override table = "Groups";
@@ -11,6 +13,12 @@ export class DbGroup extends Model {
   };
   static displayname() {
     return this.hasOne(DbIdDisplayname) as Promise<DbIdDisplayname>;
+  }
+  static knownServices() {
+    return this.hasMany(DbGroupService) as Promise<Model[]>;
+  }
+  static authorizedUsers() {
+    return this.hasMany(DbUserGroup) as Promise<Model[]>;
   }
   static async receivedInvitations() {
     const id = (await this.first()).id?.toString() ?? "";
