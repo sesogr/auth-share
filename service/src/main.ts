fee4569225eeeeb4789347df4f192afc0b4ced23
vhusername123 <17260044+mauer01@users.noreply.github.com>
2025-10-22 10:07:01 +0200

    add potential port to Connector

--- service/src/main.ts
+++ service/src/main.ts
@@ -29,6 +29,7 @@ const db = new Database(
     host: Deno.env.get("DB_HOST")!,
     username: Deno.env.get("DB_USER")!,
     password: Deno.env.get("DB_PASSWORD")!,
+    port: Number(Deno.env.get("DB_PORT")) ?? undefined,
   }),
 );
 setupManyToMany();
