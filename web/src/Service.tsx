8e138565bee3437c0fbb3a1060da6fde33f758ab
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-10-24 12:38:22 +0200

    fix red underlining adding "!"

--- web/src/Service.tsx
+++ web/src/Service.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { ConvertedService } from "./types/types.ts";
 
 const Service: React.FC<{ service: ConvertedService }> = ({ service }) => {
+  console.log(service);
   return (
     <div>
       <h2>{service.serviceName}</h2>
@@ -9,25 +10,25 @@ const Service: React.FC<{ service: ConvertedService }> = ({ service }) => {
         <li>
           Groups:{" "}
           <ul>
-            {service.groups.map((e, i) => <li key={i}>{e}</li>)}
+            {service.groups!.map((e, i) => <li key={i}>{e}</li>)}
           </ul>
         </li>
         <li>
           Owner:{" "}
           <ul>
-            {service.owners.map((e, i) => <li key={i}>{e}</li>)}
+            {service.owners!.map((e, i) => <li key={i}>{e}</li>)}
           </ul>
         </li>
         <li>
           Sent Invitations:{" "}
           <ul>
-            {service.sentInvitations.map((e, i) => <li key={i}>{e}</li>)}
+            {service.sentInvitations!.map((e, i) => <li key={i}>{e}</li>)}
           </ul>
         </li>
         <li>
           Users:{" "}
           <ul>
-            {service.users.map((e, i) => <li key={i}>{e}</li>)}
+            {service.users!.map((e, i) => <li key={i}>{e}</li>)}
           </ul>
         </li>
       </ul>
