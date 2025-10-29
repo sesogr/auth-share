38dfc6a3589e23f65c28f2f04a52d5662b29e197
vhusername123 <17260044+mauer01@users.noreply.github.com>
2025-10-29 11:12:21 +0100

    refactor types into send type and received type

--- web/src/types/ConvertedGroup.ts
+++ web/src/types/ConvertedGroup.ts
@@ -1,10 +1,15 @@
-export type ConvertedGroup = {
+import type { AllOptional } from "./AllOptional.ts";
+
+type ConvertedGroup = {
   groupname: string;
   owner: string;
-} & { [k in GroupListProperties]?: string[] };
+} & { [k in GroupListProperties]: string[] };
 
 type GroupListProperties =
   | "users"
   | "serviceList"
   | "sentInvitations"
   | "serviceInvitations";
+
+export type ReceivedConvertedGroup = ConvertedGroup;
+export type SendingConvertedGroup = AllOptional<ConvertedGroup>;
