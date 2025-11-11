b212826d6a15ba6c85f7e9767a1dbe08bbd00c75
Pu1710 <226041369+Pu1710@users.noreply.github.com>
2025-11-11 12:25:57 +0100

    add new Database field "id", set primary key and overwrite type

--- service/src/classes/Repositories/DenoDB/Models/DbGroupService.ts
+++ service/src/classes/Repositories/DenoDB/Models/DbGroupService.ts
@@ -1,4 +1,4 @@
-import { Model, Relationships } from "@denodb";
+import { DataTypes, Model, Relationships } from "@denodb";
 import { DbGroup } from "./DbGroup.ts";
 import { DbService } from "./DbService.ts";
 
@@ -9,4 +9,8 @@ export function setupGroupService() {
     DbGroup,
     DbService,
   );
+  DbGroupService.fields = {
+    ...DbGroupService.fields,
+    id: { primaryKey: true, type: DataTypes.STRING },
+  };
 }
