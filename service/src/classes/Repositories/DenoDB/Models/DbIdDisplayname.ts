9e3e846a25dea095ac89bcad964f081c2ed58abf
vhusername123 <17260044+mauer01@users.noreply.github.com>
2025-11-12 21:34:51 +0100

    camelCase to snake_case for table name aswell

--- service/src/classes/Repositories/DenoDB/Models/DbIdDisplayname.ts
+++ service/src/classes/Repositories/DenoDB/Models/DbIdDisplayname.ts
@@ -30,7 +30,9 @@ class DbAliasHelper extends DbIdDisplayname {
   static override field(field: string): string;
   static override field(field: string, nameAs: string): FieldAlias;
   static override field(field: string, nameAs?: string): string | FieldAlias {
-    const newLocal = `${this.alias}.${field}`;
+    const newLocal = `${this.alias}.${
+      field.replace(/[A-Z]/g, (ch) => `_${ch.toLowerCase()}`)
+    }`;
     field = field.replace(/_([a-z])/g, (_, ch) => ch.toUpperCase());
     if (!Object.keys(this.fields).includes(field)) {
       throw new Error("Field does not Exist!");
--- service/src/classes/Repositories/DenoDB/Models/DbInvitation.ts
+++ service/src/classes/Repositories/DenoDB/Models/DbInvitation.ts
@@ -28,7 +28,9 @@ class DbJoinHelper extends DbInvitation {
   static override field(field: string): string;
   static override field(field: string, nameAs: string): FieldAlias;
   static override field(field: string, nameAs?: string): string | FieldAlias {
-    const newLocal = `${this.alias}.${field}`;
+    const newLocal = `${this.alias}.${
+      field.replace(/[A-Z]/g, (ch) => `_${ch.toLowerCase()}`)
+    }`;
     field = field.replace(/_([a-z])/g, (_, ch) => ch.toUpperCase());
     if (!Object.keys(this.fields).includes(field)) {
       throw new Error("Field does not Exist!");
