cd8158d5589fe26d1746fdf7ee92ea15770d762f
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-12-18 13:02:38 +0100

    extend from Headcontroller to get ME dynamically

--- service/src/controller/ServiceController.ts
+++ service/src/controller/ServiceController.ts
@@ -4,21 +4,23 @@ import { UserRepository } from "../interfaceTypes/UserRepository.ts";
 import { Context } from "@hono/hono";
 import { Service } from "../classes/Service.ts";
 import { ServiceCredential } from "../classes/ServiceCredential.ts";
-import { User } from "../classes/User.ts";
+import { HeadController } from "./HeadController.ts";
 
-export class ServiceController {
+export class ServiceController extends HeadController {
   constructor(
     private readonly serviceRepository: ServiceRepository,
-    private readonly userRepo: UserRepository,
-    private readonly ME: User,
-  ) {}
+    userRepository: UserRepository,
+  ) {
+    super(userRepository);
+  }
 
   async listMyServices(
     c: Context,
   ) {
     try {
+      const ME = await this.getMeFromContext(c);
       const serviceList = await this.serviceRepository.findOwnedByUserId(
-        this.ME.getId(),
+        ME.getId(),
       );
       const convertedList: ConvertedService[] = serviceList.map((e) =>
         e.toJson()
@@ -31,6 +33,7 @@ export class ServiceController {
   }
   async add(c: Context) {
     try {
+      const ME = await this.getMeFromContext(c);
       const convertedService: ConvertedService = await c.req!.json!();
       const service = Service.createService(
         new ServiceCredential(
@@ -39,7 +42,7 @@ export class ServiceController {
         ),
         convertedService.serviceName,
         convertedService.serviceUrl,
-        this.ME,
+        ME,
       ); //Todo: new = new type(arguments);, convertedService.serviceName)
       await this.serviceRepository.save(service);
       return c.body!(null, 201);
