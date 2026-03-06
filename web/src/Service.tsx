f58dcbf49510e94a834635fb8bcd326f77380a2a
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2026-03-06 16:58:38 +0100

    add front end possibility to test

--- web/src/Service.tsx
+++ web/src/Service.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { Collapse, List, Typography } from "antd";
-import type { ReceivedConvertedService } from "./types/ConvertedService.ts";
+import React, { useState } from "react";
+import { Button, Collapse, Input, List, Typography } from "antd";
+import type { ReceivingConvertedService } from "./types/types.ts";
 
 const { Panel } = Collapse;
 const { Title } = Typography;
 
-const Service: React.FC<{ service: ReceivedConvertedService }> = (
+const Service: React.FC<{ service: ReceivingConvertedService }> = (
   { service },
 ) => {
+  const [value, setValue] = useState<string>("");
   return (
     <div style={{ padding: 24 }}>
       <Title level={3}>{service.serviceName}</Title>
@@ -42,7 +43,28 @@ const Service: React.FC<{ service: ReceivedConvertedService }> = (
             dataSource={service.users}
             renderItem={(item) => <List.Item>{item}</List.Item>}
           />
+          <Input onChange={(e) => setValue(e.target.value)} />
+          <Button
+            onClick={() => {
+              service.users.push(value);
+              console.log(service.users);
+            }}
+          >
+            Add
+          </Button>
         </Panel>
+        <Button
+          onClick={() => {
+            fetch(import.meta.env.VITE_APIURL + "/service/users", {
+              method: "PATCH",
+              headers: { "Content-Type": "application/json" },
+              credentials: "include",
+              body: JSON.stringify(service),
+            }).then(async (e) => console.log(await e.json()));
+          }}
+        >
+          Confirm Changes
+        </Button>
       </Collapse>
     </div>
   );
