25f3747762a814b916d4a34c08b7856348d7564c
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2026-03-06 16:58:18 +0100

    add addUsersToService controller

--- service/src/controller/ServiceController.ts
+++ service/src/controller/ServiceController.ts
@@ -7,11 +7,18 @@ import { Context } from "@hono/hono";
 import { Service } from "../classes/Service.ts";
 import { ServiceCredential } from "../classes/ServiceCredential.ts";
 import { HeadController } from "./HeadController.ts";
-import { AuthorizationError } from "../errors/controllerErrors/UnauthorizedError.ts";
+import { AuthorizationError } from "../errors/controllerErrors/AuthorizationError.ts";
+import { UserRepository } from "../interfaceTypes/UserRepository.ts";
+import { User } from "../classes/User.ts";
+import { NotFoundError } from "../errors/NotFoundError.ts";
+import { PromisesUtil } from "../services/PromissesUtil.ts";
+import { ItemAlreadyExistsError } from "../errors/ItemAlreadyExistsError.ts";
+import { ConvertedUser } from "../types/types.ts";
 
 export class ServiceController extends HeadController {
   constructor(
     private readonly serviceRepository: ServiceRepository,
+    private readonly userRepository: UserRepository,
   ) {
     super();
   }
@@ -56,11 +63,54 @@ export class ServiceController extends HeadController {
       this.errorHandle(error, c);
     }
   }
+  async addUsersToService(c: Context) {
+    try {
+      const ME = this.getMeFromContext(c);
+      const serviceData: ConvertedService = await c.req.json();
+      ensureConvertedServiceIntegrity(serviceData, ["id", "users"]);
+      const service: Service = await this.serviceRepository.findById(
+        serviceData.id,
+      );
+      service.checkOwner(ME);
+      const settledRecords: PromiseSettledResult<User>[] = await Promise
+        .allSettled(
+          serviceData.users.map((e) =>
+            this.userRepository.findByDisplayName(e)
+          ),
+        );
+      const { fullfilled: users, rejected: notfound } = PromisesUtil
+        .splitSettled<
+          User,
+          NotFoundError
+        >(settledRecords);
+      const alreadyAuthorized: ConvertedUser[] = [];
+      const fullfilledUsers: ConvertedUser[] = [];
+      users.forEach((u) => {
+        try {
+          service.giveAuthorizationToUser(u);
+          fullfilledUsers.push(u.toJson());
+        } catch (error) {
+          if (error instanceof ItemAlreadyExistsError) {
+            alreadyAuthorized.push(u.toJson());
+          }
+        }
+      });
+      await this.serviceRepository.save(service);
+      return c.json({
+        resolved: fullfilledUsers,
+        alreadyIn: alreadyAuthorized,
+        rejected: notfound,
+      }, 200);
+    } catch (error) {
+      return this.errorHandle(error, c);
+    }
+  }
+
   async delete(c: Context) {
     try {
       const ME = this.getMeFromContext(c);
       const convertedService: ConvertedService = await c.req.json();
-      ensureConvertedServiceIntegrity(convertedService);
+      ensureConvertedServiceIntegrity(convertedService, "id");
       const service = await this.serviceRepository.findById(
         convertedService.id,
       );
