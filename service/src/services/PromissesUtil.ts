fca98886c74eb4260682b32c1693c4f04397945d
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2026-03-09 15:13:03 +0100

    fix typo restraining ERROR Type in PromisesUtil

--- service/src/services/PromissesUtil.ts
+++ service/src/services/PromissesUtil.ts
@@ -1,16 +1,16 @@
 export class PromisesUtil {
-  static splitSettled<VALUE, ERROR>(
-    settled: PromiseSettledResult<VALUE>[],
-  ): { fullfilled: VALUE[]; rejected: ERROR[] } {
-    const items: VALUE[] = [];
-    const errors: ERROR[] = [];
-    for (const record of settled) {
-      if (record.status === "fulfilled") {
-        items.push(record.value);
-      } else {
-        errors.push(record.reason);
-      }
+    static splitSettled<VALUE, ERROR extends Error>(
+        settled: PromiseSettledResult<VALUE>[],
+    ): { fulfilled: VALUE[]; rejected: ERROR[] } {
+        const items: VALUE[] = [];
+        const errors: ERROR[] = [];
+        for (const record of settled) {
+            if (record.status === "fulfilled") {
+                items.push(record.value);
+            } else {
+                errors.push(record.reason);
+            }
+        }
+        return {fulfilled: items, rejected: errors};
     }
-    return { fullfilled: items, rejected: errors };
-  }
 }
