7d7a5c110cc567251c764c0036abafeefa0649b9
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2026-03-19 19:58:02 +0100

    fix Database Invitation Layout

--- service/src/classes/Repositories/DenoDB/Models/DbInvitation.ts
+++ service/src/classes/Repositories/DenoDB/Models/DbInvitation.ts
@@ -1,31 +1,35 @@
-import { DataTypes } from "@denodb";
-import { UniqueNumber } from "../UniqueNumber.ts";
-import { DbAliasableModel } from "./DbAliasableModel.ts";
+import {DataTypes} from "@denodb";
+import {UniqueNumber} from "../UniqueNumber.ts";
+import {DbAliasableModel} from "./DbAliasableModel.ts";
 
 export class DbInvitation extends DbAliasableModel {
-  static override table = "Invitations";
-  static override timestamps = true; //needed?
-  static override fields = {
-    senderReference: DataTypes.STRING,
-    objReference: DataTypes.STRING,
-    receiverReference: DataTypes.STRING,
-  };
+    static override table = "Invitations";
+    static override timestamps = true; //needed?
+    static override fields = {
+        id: {type: DataTypes.STRING, primaryKey: true},
+        senderReference: DataTypes.STRING,
+        objReference: DataTypes.STRING,
+        receiverReference: DataTypes.STRING,
+    };
 }
+
 
 export class DbInvitationJoinOnSender extends DbInvitation {
-  protected static override alias = UniqueNumber.next() + "";
-  static override table = super.table + " AS " + this.alias;
+    protected static override alias = UniqueNumber.next() + "";
+    static override table = super.table + " AS " + this.alias;
 }
 
+
 export class DbInvitationJoinOnReceived extends DbInvitation {
-  protected static override alias = UniqueNumber.next() + "";
-  static override table = super.table + " AS " + this.alias;
+    protected static override alias = UniqueNumber.next() + "";
+    static override table = super.table + " AS " + this.alias;
 }
 
 export class DbInvitationJoinOnObject extends DbInvitation {
-  protected static override alias = UniqueNumber.next() + "";
-  static override table = super.table + " AS " + this.alias;
+    protected static override alias = UniqueNumber.next() + "";
+    static override table = super.table + " AS " + this.alias;
 }
+
 // Relationships.hasMany(DbInvitation, DbUser);
 // Relationships.hasMany(DbInvitation, DbGroup);
 // Relationships.hasMany(DbInvitation, DbService);
