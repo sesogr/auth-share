bab86077e71ab8b3c1e9949705f2e7ef33e4318e
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2026-03-20 14:13:36 +0100

    add group repo in service controller test

--- service/test/ControllerTests/ServiceController_test.ts
+++ service/test/ControllerTests/ServiceController_test.ts
@@ -5,13 +5,15 @@ import { UserRepository } from "../../src/interfaceTypes/UserRepository.ts";
 import { stub } from "@std/testing/mock";
 import { User } from "../../src/classes/User.ts";
 import { Service } from "../../src/classes/Service.ts";
+import { GroupRepository } from "../../src/interfaceTypes/GroupRepository.ts";
 
 Deno.test("DbServiceController", async (t) => {
+  const { serviceController, mockContext } = buildUp();
   await t.step("list my Service", async () => {
-    const { serviceController, mockContext } = buildUp();
     const _serviceList =
       (await serviceController.listMyServices(mockContext)) as unknown as [];
-    console.log(_serviceList);
+  });
+  await t.step("change password", () => {
   });
 
   function buildUp() {
@@ -22,9 +24,11 @@ Deno.test("DbServiceController", async (t) => {
     } as ServiceRepository;
     const userRepo = {} as UserRepository;
     const user = { getId: () => "123" } as User;
+    const groupRepo = {} as GroupRepository;
     const serviceController = new ServiceController(
       serviceRepo,
       userRepo,
+      groupRepo,
     );
     const _getMeFromContext = stub(
       serviceController,
