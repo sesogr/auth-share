167e0c984cf5824874f54b298ca20835a03415fe
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2026-04-09 13:31:39 +0200

    ram only log for test

--- service/test/RamOnlyLog.ts
+++ service/test/RamOnlyLog.ts
@@ -1,22 +1,22 @@
-import { Logger } from "../interfaceTypes/Logger.ts";
+import { Logger, LogLevel } from "../interfaceTypes/Logger.ts";
 
 export class RamOnlyLog implements Logger {
   constructor(readonly logLevel = [], readonly context = "") {
   }
-  logList: unknown[] = [];
+  logList: [LogLevel, unknown[], string][] = [];
   withOwnContext(context: string): Logger {
     return new RamOnlyLog(this.logLevel, context);
   }
   log(...message: unknown[]): void {
-    this.logList.push("info", message);
+    this.logList.push(["info", message, this.context]);
   }
   warn(...message: unknown[]): void {
-    this.logList.push("warn", message);
+    this.logList.push(["warn", message, this.context]);
   }
   error(...message: unknown[]): void {
-    this.logList.push("error", message);
+    this.logList.push(["error", message, this.context]);
   }
-  debug(): void {
-    this.logList.push("debug", arguments);
+  debug(...message: unknown[]): void {
+    this.logList.push(["debug", message, this.context]);
   }
 }
