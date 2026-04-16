2ef37cc36607cadcabeec36e25157f03168d5caa
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2026-04-16 22:12:45 +0200

    enhance TestUser with TestCredentials and fix missing stub override reset for credentials

--- service/test/StubbedClasses/TestUser.ts
+++ service/test/StubbedClasses/TestUser.ts
@@ -1,19 +1,21 @@
 import { CustomTestStub } from "../CustomTestStub.ts";
-import { User } from "../../src/classes/Entities/User.ts";
+import type { User } from "../../src/classes/Entities/User.ts";
 
 export class TestUser extends CustomTestStub<User> {
   static override create<T = User>(): T & TestUser {
     return new TestUser() as T & TestUser;
   }
+  readonly _credentials: TestCredentials;
   private constructor() {
     super();
+    this._credentials = TestCredentials.create();
     this.initializeStub("getId");
     this.initializeStub("getDisplayName");
     this.initializeStub("toJsonString");
     this.initializeStub("toJson");
     this.initializeStub("convertToShort");
     this.initializeStub("validateSession");
-    this.initializeStub("getCredentials");
+    this.initializeStub("getCredentials", () => this._credentials);
     this.initializeStub("changeUserCredentials");
     this.initializeStub("deleteSession");
     this.initializeStub("deleteSessionByToken");
@@ -23,5 +25,26 @@ export class TestUser extends CustomTestStub<User> {
     this.initializeStub("removeInvitation");
     this.initializeStub("listUserGroupInvitation");
     this.initializeStub("setDisplayName");
+    this.initializeStub("checkValidation");
+  }
+  override reset(trueReset: boolean = false) {
+    this._credentials.reset(trueReset);
+    super.reset(trueReset);
+  }
+}
+
+class TestCredentials extends CustomTestStub<User["credentials"]> {
+  private constructor() {
+    super();
+    this.initializeStub("copy");
+    this.initializeStub("equals");
+    this.initializeStub("toString");
+    this.initializeStub("with");
+    this.initializeStub("verifyPasswordHash");
+    this.initializeStub("changePassword");
+    this.initializeStub("assertsVerification");
+  }
+  static override create<T = User["credentials"]>(): T & TestCredentials {
+    return new TestCredentials() as T & TestCredentials;
   }
 }
