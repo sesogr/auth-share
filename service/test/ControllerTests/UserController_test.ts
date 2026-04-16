d5303c7d5c7350ea82882688ec8faabdd7e8f0b6
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2026-04-16 22:14:50 +0200

    tests for authMiddleware, listMyServices, read, changePassword, and logOut methods

--- service/test/ControllerTests/UserController_test.ts
+++ service/test/ControllerTests/UserController_test.ts
@@ -1,137 +1,134 @@
-import { spy, stub } from "@std/testing/mock";
 import { UserController } from "../../src/classes/controller/UserController.ts";
-import { assertEquals } from "@std/assert";
-import { FakeObjectGen } from "../../src/classes/FakeObjectGen.ts";
-import { Context } from "@hono/hono";
-import { UserRepository } from "../../interfaceTypes/UserRepository.ts";
-import { UserCredential } from "../../src/classes/Values/UserCredential.ts";
-import { ConvertedUser } from "../../src/types/ConvertedUser.ts";
-import { User } from "../../src/classes/Entities/User.ts";
 import { RamOnlyLog } from "../RamOnlyLog.ts";
-
-const context = {
-  res: {
-    headers: {
-      set: () => true,
-    },
-  },
-  req: {
-    json: () => {
-      return Promise.resolve({
-        displayname: "Hans Maiser",
-        credentials: {
-          username: "Hans Maiser",
-          password: "wh234he",
-        },
-      } as ConvertedUser);
-    },
-  },
-  //@ts-ignore any parameter
-  body: (a, b) => {
-    return { body: a, status: b };
-  },
-  //@ts-ignore any parameter
-  json: (a, b) => context.body(a, b),
-} as unknown as Context;
+import { TestUserRepo } from "../StubbedClasses/TestUserRepo.ts";
+import { TestContext } from "../StubbedClasses/TestContext.ts";
+import HonoCookieAdapter from "../../src/adapter/HonoCookieAdapter.ts";
+import { stub } from "@std/testing/mock";
+import { TestUser } from "../StubbedClasses/TestUser.ts";
+import { assertEquals } from "@std/assert";
+import { ConvertedUser } from "../../src/types/ConvertedUser.ts";
 
 Deno.test("UserController - Test", async (t) => {
-  const ramLogger = new RamOnlyLog();
-  const logger = ramLogger;
-  await t.step("Create", async (st) => {
-    await st.step("user is saved", async () => {
-      const repo = {
-        save: () => Promise.resolve(),
-      } as unknown as UserRepository;
-      const controller = new UserController(repo, logger);
-      const spySave = spy(repo, "save");
-      const response = await controller.create(context);
-      assertEquals(response?.body, null);
-      assertEquals(response?.status, 201);
-      assertEquals(spySave.calls.length, 1);
-      assertEquals(
-        spySave.calls[0].args[0].getCredentials().username,
-        "Hans Maiser",
-      );
+  const goodReturn = "returned";
+  const userRepo = TestUserRepo.create();
+  const userController = new UserController(userRepo, new RamOnlyLog());
+  const errorReturned = "errorReturned";
+  //@ts-ignore protected member
+  const errorHandleStub = stub(userController, "errorHandle", () => {
+    return errorReturned;
+  });
+  const mockContext = TestContext.create();
+  const mockedUser = TestUser.create();
+  mockContext.registerOutput("get", mockedUser, true);
+  let sessionToken: string;
+  const cookieStub = stub(
+    HonoCookieAdapter,
+    "saveGetCookie",
+    () => sessionToken,
+  );
+  await t.step("authMiddleware", async (st) => {
+    await st.step("getCookie returns a session token", async () => {
+      userRepo.reset();
+      mockContext.reset();
+      mockedUser.reset();
+      sessionToken = "123";
+      userRepo.registerOutput("findBySessionToken", mockedUser);
+      let nextTriggered = false;
+      await userController.authMiddleware(mockContext, () => {
+        nextTriggered = true;
+        return Promise.resolve();
+      });
+      assertEquals(nextTriggered, true);
+      assertEquals(cookieStub.calls[0].args, [mockContext, "session"]);
+      assertEquals(mockedUser.stub["validateSession"].args[0], [sessionToken]);
+      assertEquals(userRepo.stub["findBySessionToken"].args[0], [sessionToken]);
     });
-    await st.step("invalid request data", async () => {
-      const controller = new UserController(
-        {} as unknown as UserRepository,
-        ramLogger,
-      );
-      const contextWithInvalidData = {
-        ...context,
-        req: {
-          json: () => {
-            return { "ha": "be" } as unknown as Promise<ConvertedUser>;
-          },
-        },
-      } as unknown as Context;
-
-      const returnBody = await controller.create(contextWithInvalidData);
-      assertEquals(returnBody.status, 400);
-      //@ts-ignore body is different
-      assertEquals(returnBody.body.message, "Wrong Keys Detected");
+    await st.step("getCookie returns null", async () => {
+      userRepo.reset();
+      mockContext.reset();
+      mockedUser.reset();
+      sessionToken = "";
+      userRepo.registerOutput("findBySessionToken", mockedUser);
+      let nextTriggered = false;
+      const returned = await userController.authMiddleware(mockContext, () => {
+        nextTriggered = true;
+        return Promise.resolve();
+      }) as unknown as string;
+      assertEquals(returned, errorReturned);
+      assertEquals(nextTriggered, false);
+      assertEquals(cookieStub.calls[0].args, [mockContext, "session"]);
+      assertEquals(errorHandleStub.calls[0].args[1], mockContext);
+      assertEquals(userRepo.counter("findBySessionToken"), 0);
     });
   });
-  await t.step("Change Password", async (st) => {
-    await st.step("password is changed", async () => {
-      const repo = {
-        findById: (id: string) => {
-          const testUser = {
-            changeUserCredentials: (a: UserCredential) => {
-              testUser.username = a.username;
-              testUser.password = a.hash;
-            },
-            username: "",
-            password: "",
-            getId: () => id,
-            //getCredentials: () => testUser
-            getCredentials: () => {
-              return {
-                changePassword: (newPassword: string) => {
-                  testUser.username = newPassword.split(":")[0];
-                  testUser.password = newPassword.split(":")[1];
-                },
-                username: testUser.username,
-                password: testUser.password,
-              };
-            },
-          };
-          return Promise.resolve(testUser);
-        },
-        save: () => Promise.resolve(),
-      } as unknown as UserRepository;
-
-      //initieren des zu testenden Objects
-      const controller = new UserController(repo, logger);
-      const spySave = spy(repo, "save");
-      const user: User = await FakeObjectGen.createFakeUser();
-      const stubChangeUserCredentials = stub(
-        user,
-        "changeUserCredentials",
-        (a: UserCredential) => {
-          //@ts-ignore override private member
-          user.getCredentials = () => a;
-        },
-      );
-      //@ts-ignore protected member
-      const stubGetMe = stub(controller, "getMeFromContext", () => {
-        //@ts-ignore override private method
-        user.validateSession = () => user.validated = true;
-        user.validateSession("");
-        return user;
+  await t.step("listMyServices", () => {
+    mockedUser.reset();
+    mockContext.reset();
+    const listServiceReturn = ["asd"];
+    mockedUser.registerOutput("listServices", listServiceReturn);
+    mockContext.registerOutput("json", goodReturn);
+    const returned = userController.listMyServices(
+      mockContext,
+    ) as unknown as string;
+    assertEquals(returned, goodReturn);
+    assertEquals(mockedUser.stub["listServices"].args[0], []);
+    assertEquals(mockContext.lastArgs("json"), [listServiceReturn]);
+  });
+  await t.step("read", () => {
+    mockedUser.reset();
+    mockContext.reset();
+    mockContext.registerOutput("json", goodReturn);
+    mockedUser.registerOutput("toJson", { asd: "asd" });
+    const returned = userController.read(mockContext) as unknown as string;
+    assertEquals(returned, goodReturn);
+    assertEquals(mockedUser.stub["toJson"].args[0], []);
+    assertEquals(mockContext.lastArgs("json"), [{ asd: "asd" }]);
+  });
+  await t.step("change Password", async (st) => {
+    await st.step("all good", async () => {
+      const changedPassword = "changedPassword";
+      mockedUser.reset();
+      mockContext.reset();
+      userRepo.reset();
+      const password = "abc";
+      mockContext.req.registerOutput("json", {
+        credentials: {
+          username: "not a password",
+          password: password,
+        } as ConvertedUser["credentials"],
       });
-      //start der zu testenden Methode mit folgenden assertions
-      const response = await controller.changePassword(context);
-      assertEquals(response?.body, null);
-      assertEquals(response?.status, 204);
-      assertEquals(spySave.calls.length, 1);
-      assertEquals(stubGetMe.calls.length, 1);
-      assertEquals(
-        spySave.calls[0].args[0].getCredentials().hash,
-        stubChangeUserCredentials.calls[0].args[0].hash,
-      );
-      assertEquals(spySave.calls[0].args[0].getId(), user.getId());
+      mockedUser._credentials.registerOutput("changePassword", changedPassword);
+      mockContext.registerOutput("body", goodReturn);
+      const returned = await userController.changePassword(
+        mockContext,
+      ) as unknown as string;
+      assertEquals(returned, goodReturn);
+      assertEquals(mockedUser._credentials.stub["changePassword"].args[0], [
+        password,
+      ]);
+      assertEquals(mockContext.lastArgs("body"), [null, 204]);
+      assertEquals(userRepo.lastArgs("save"), [mockedUser]);
     });
+  });
+  await t.step("log out", async () => {
+    mockedUser.reset();
+    mockContext.reset();
+    userRepo.reset();
+    sessionToken = "123";
+    const deleteCookieStub = stub(HonoCookieAdapter, "deleteCookie");
+    userRepo.registerOutput("findBySessionToken", mockedUser);
+    mockContext.registerOutput("body", goodReturn);
+    const returned = await userController.logOut(
+      mockContext,
+    ) as unknown as string;
+    assertEquals(returned, goodReturn);
+    assertEquals(userRepo.lastArgs("findBySessionToken"), [sessionToken]);
+    assertEquals(mockedUser.stub["deleteSessionByToken"].args[0], [
+      sessionToken,
+    ]);
+    assertEquals(cookieStub.calls[2].args, [mockContext, "session"]);
+    assertEquals(deleteCookieStub.calls[0].args, [mockContext, "session"]);
+    assertEquals(mockContext.lastArgs("body"), [null, 200]);
+    assertEquals(userRepo.lastArgs("save"), [mockedUser]);
   });
 });
